6fa7368a33149f307ac64a0bd153d24bc03e18cf
erpranto <erpranto55@gmail.com>
2025-11-05 22:36:16 +0600

    5/11/2025-Starter-211

---
+++ Contest/Starters-211/Fuel_Check.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int x, y;
+    cin >> x >> y;
+    if (x * y < 100)
+        cout << "No\n";
+    else
+        cout << "Yes\n";
+    return 0;
+}
